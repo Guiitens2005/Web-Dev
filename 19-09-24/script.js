d7bca359fd29a4035fa3145c8f58417098cb58c0
Guiitens <guilherme.a.caseiro@gmail.com>
2024-09-19 11:46:03 -0300

    Add files via upload
    código a finalizar

---
+++ 19-09-24/script.js
@@ -0,0 +1,30 @@
+
+alert("Olá mundo - Externo")
+let nome3 = "Guilherme"
+console.log(nome3)
+
+let idade = "18";
+console.log(idade)
+
+let valor = 6.40;
+console.log(valor)
+
+let texto = "fiap";
+console.log(texto)
+
+let aluno = true
+console.log(aluno)
+
+let tecnologias = ["JAVA, C#, HTML, CSS"]
+
+let numeroFloat = 123.456;
+console.log(parseInt(numeroFloat));
+
+let numeroString = "654.321"
+console.log(parseFloat(numeroString));
+
+let numeroInteiro = 789
+console.log(numeroInteiro.toString())
+
+let verTipo = typeof alunoAula
+console.log(verTipo)
